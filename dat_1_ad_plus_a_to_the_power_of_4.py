bd46576b56facbd9bbb2011e61cc9a7abf52792e
Ryui Kaneko <27846552+ryuikaneko@users.noreply.github.com>
2022-08-08 13:04:51 +0900

    add codes

---
+++ dat_1_ad_plus_a_to_the_power_of_4.py
@@ -0,0 +1,1 @@
+3 + 12*Dagger(a)*a + 4*Dagger(a)*a**3 + 6*Dagger(a)**2 + 6*Dagger(a)**2*a**2 + 4*Dagger(a)**3*a + Dagger(a)**4 + 6*a**2 + a**4
---
+++ test_1_ad_plus_a_to_the_power_of_4.py
@@ -0,0 +1,103 @@
+## https://stackoverflow.com/questions/59523322/in-sympy-simplify-an-expression-using-a-canonical-commutation-relation
+## https://stackoverflow.com/questions/53269231/sympy-substitute-to-symbolic-bosonic-commutation-its-numerical-value
+
+import sympy
+from sympy import *
+from sympy.physics.quantum import *
+from sympy.physics.secondquant import *
+
+from sympy.core.operations import AssocOp
+
+def apply_ccr(expr, ccr, reverse=False):
+    if not isinstance(expr, Basic):
+        raise TypeError("The expression to simplify is not a sympy expression.")
+
+    if not isinstance(ccr, Eq):
+        if isinstance(ccr, Basic):
+            ccr = Eq(ccr, 0)
+        else:
+            raise TypeError("The canonical commutation relation is not a sympy expression.")
+
+    comm = None
+
+    for node in preorder_traversal(ccr):
+        if isinstance(node, Commutator):
+            comm = node
+            break
+
+    if comm is None:
+        raise ValueError("The cannonical commutation relation doesn not include a commutator.")
+
+    solutions = solve(ccr, comm)
+
+    if len(solutions) != 1:
+        raise ValueError("There are more solutions to the cannonical commutation relation.")
+
+    value = solutions[0]
+
+    A = comm.args[0]
+    B = comm.args[1]
+
+    if reverse:
+        (A, B) = (B, A)
+        value = -value
+
+    def is_expandable_pow_of(base, expr):
+        return isinstance(expr, Pow) \
+            and base == expr.args[0] \
+            and isinstance(expr.args[1], Number) \
+            and expr.args[1] >= 1
+
+
+    def walk_tree(expr):
+        if isinstance(expr, Number):
+            return expr
+
+        if not isinstance(expr, AssocOp) and not isinstance(expr, Function):
+            return expr.copy()
+
+        elif not isinstance(expr, Mul):
+            return expr.func(*(walk_tree(node) for node in expr.args))
+
+        else:
+            args = [arg for arg in expr.args]
+
+            for i in range(len(args)-1):
+                x = args[i]
+                y = args[i+1]
+
+                if B == x and A == y:
+                    args = args[0:i] + [A*B - value] + args[i+2:]
+                    return walk_tree( Mul(*args).expand() )
+
+                if B == x and is_expandable_pow_of(A, y):
+                    ypow = Pow(A, y.args[1] - 1)
+                    args = args[0:i] + [A*B - value, ypow] + args[i+2:]
+                    return walk_tree( Mul(*args).expand() )
+
+                if is_expandable_pow_of(B, x) and A == y:
+                    xpow = Pow(B, x.args[1] - 1)
+                    args = args[0:i] + [xpow, A*B - value] + args[i+2:]
+                    return walk_tree( Mul(*args).expand() )
+
+                if is_expandable_pow_of(B, x) and is_expandable_pow_of(A, y):
+                    xpow = Pow(B, x.args[1] - 1)
+                    ypow = Pow(A, y.args[1] - 1)
+                    args = args[0:i] + [xpow, A*B - value, ypow] + args[i+2:]
+                    return walk_tree( Mul(*args).expand() )
+
+            return expr.copy()
+
+
+    return walk_tree(expr)
+
+Basic.apply_ccr = lambda self, ccr, reverse=False: apply_ccr(self, ccr, reverse)
+
+#----
+
+a = Operator('a')
+ad = Dagger(a)
+ccr = Eq( Commutator(a, ad),  1 )
+
+ret = ((ad + a)**4).expand().apply_ccr(ccr)
+print(ret)
